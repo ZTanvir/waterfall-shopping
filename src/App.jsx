a75282b0e029cb6abed096b5a7d0dc9fe7140d76
zahirul <zahirulopel@gmail.com>
2025-02-05 10:31:39 +0600

    add content to Home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,19 @@
 import { Routes, Route } from "react-router-dom";
 import "./App.css";
-import Navbar from "./components/Navbar";
-import Footer from "./components/Footer";
 import Home from "./page/Home";
 import Shop from "./page/Shop";
 import Contacts from "./page/Contacts";
 import Login from "./page/Login";
-import ProductCard from "./components/ProductCard";
 
 function App() {
   return (
     <>
-      <Navbar />
-      <div className="products">
-        <ProductCard
-          productImg="https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg"
-          name="Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops"
-          price="109.95"
-        />
-        <ProductCard
-          productImg="https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg"
-          name="Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops"
-          price="109.95"
-        />
-        <ProductCard
-          productImg="https://fakestoreapi.com/img/71-3HjGNDUL._AC_SY879._SX._UX._SY._UY_.jpg"
-          name="Fjallraven"
-          price="109.95"
-        />
-      </div>
-
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/shop" element={<Shop />} />
         <Route path="/contacts" element={<Contacts />} />
         <Route path="/login" element={<Login />} />
       </Routes>
-      <Footer />
     </>
   );
 }
--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -1,7 +1,32 @@
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
+import Btn from "../components/Btn";
+import ProductCard from "../components/ProductCard";
+import { Link } from "react-router";
 const Home = () => {
   return (
     <>
-      <h1>Home page</h1>
+      <header>
+        <Navbar />
+      </header>
+      <main>
+        <section>
+          <h1 className="">
+            The most <span>beautiful</span> and <span>high-quality</span>
+            products in the whole world.
+          </h1>
+          <p>
+            Shop the latest trends and must-have products from the comfort of
+            your home. Fast shipping and amazing customer service guaranteed.
+          </p>
+
+          <Link to="/shop">
+            <Btn text="Explore Now" />
+          </Link>
+        </section>
+        <ProductCard />
+      </main>
+      <Footer />
     </>
   );
 };
