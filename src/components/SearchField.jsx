cdc5a219c481cf12f1f05f7544da72c0f1836814
zahirul <zahirulopel@gmail.com>
2025-02-09 12:29:41 +0600

    add searchField component

---
+++ src/components/SearchField.jsx
@@ -0,0 +1,29 @@
+import searchIconImg from "../assets/images/search-icon-white.svg";
+import styles from "../styles/components/searchField.module.css";
+const SearchField = ({ text, setText }) => {
+  return (
+    <form
+      className={styles.searchField__wrapper}
+      onSubmit={(e) => {
+        e.preventDefault();
+        setText("");
+      }}
+    >
+      <input
+        type="text"
+        name="search"
+        id="search-box"
+        value={text}
+        onChange={(e) => setText(e.target.value)}
+      />
+      <button type="submit">
+        <img
+          className={styles.searchIcon}
+          src={searchIconImg}
+          alt="magnifying glass used to search item"
+        />
+      </button>
+    </form>
+  );
+};
+export default SearchField;
--- src/page/Shop.jsx
+++ src/page/Shop.jsx
@@ -1,9 +1,11 @@
 import styles from "../styles/page/shop.module.css";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
+import SearchField from "../components/SearchField";
 import { useEffect, useState } from "react";
 const Shop = ({ cardData }) => {
   const [products, setProducts] = useState(null);
+  const [search, setSearch] = useState("");
   const { data, isLoading, isError } = cardData;
 
   const dependency = data === undefined ? data : data.length;
@@ -19,7 +21,9 @@ const Shop = ({ cardData }) => {
       <header>
         <Navbar />
       </header>
-      <main></main>
+      <main>
+        <SearchField text={search} setText={setSearch} />
+      </main>
       <Footer />
     </div>
   );
