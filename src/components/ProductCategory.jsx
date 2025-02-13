ca237e85282d94f1c1afef591bb5a7f0f143dc51
zahirul <zahirulopel@gmail.com>
2025-02-13 12:11:56 +0600

    add error msg to productCategory component

--- src/components/ProductCategory.jsx
+++ src/components/ProductCategory.jsx
@@ -4,8 +4,19 @@ import styles from "../styles/components/productCategory.module.css";
 const ProductCategory = ({ categoryName, setProduct }) => {
   const { data, isLoading, isError } =
     productService.getCategoryProducts(categoryName);
+
+  if (isError)
+    return (
+      <div className={styles.product__wrapper}>
+        <p>Error while processing the request,please refresh the page.</p>
+      </div>
+    );
+
+  const handleProductClick = (e) => {
+    setProduct(data);
+  };
   return (
-    <div className={styles.product__wrapper}>
+    <div onClick={handleProductClick} className={styles.product__wrapper}>
       <span className={styles.product__name}>{data && categoryName}</span>
       <span className={styles.product__amount}>
         {data && `(${data.length})`}
