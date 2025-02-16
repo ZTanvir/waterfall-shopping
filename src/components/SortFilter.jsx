8ec4aabaadfe4bac671e592a70f0b42bd94054b0
zahirul <zahirulopel@gmail.com>
2025-02-16 12:26:00 +0600

    Sort product based on default,rating,price

--- src/components/SortFilter.jsx
+++ src/components/SortFilter.jsx
@@ -1,37 +1,53 @@
 import SelectField from "./SelectField";
 import { useEffect, useState } from "react";
+import styles from "../styles/components/sortFilter.module.css";
 
 const SortFilter = ({ products, setProducts }) => {
   const [selectFilter, setSelectFilter] = useState("default");
-  const [a, setA] = useState(products);
+  const [initialProduct, setInitialProduct] = useState(null);
 
-  const productSize = products && products[0].id;
+  const productCategory = products && products[0].category;
 
   useEffect(() => {
     if (selectFilter === "default") {
-      console.log("update a");
-      console.log("product value:", products);
-
-      setA(products);
+      setProducts(initialProduct);
+    } else if (selectFilter === "avg-rating") {
+      let sortByRating = [...products].sort(
+        (product1, product2) => product2.rating.rate - product1.rating.rate
+      );
+      setProducts(sortByRating);
+    } else if (selectFilter === "price-low") {
+      let sortByPriceAsc = [...products].sort(
+        (product1, product2) => product1.price - product2.price
+      );
+      setProducts(sortByPriceAsc);
+    } else if (selectFilter === "price-high") {
+      let sortByPriceDsc = [...products].sort(
+        (product1, product2) => product2.price - product1.price
+      );
+      setProducts(sortByPriceDsc);
     }
-    console.log("sort result:", selectFilter, products);
-    console.log("initial product:", a);
   }, [selectFilter]);
 
   useEffect(() => {
-    if (productSize) {
-      console.log("Render sortFilter");
+    // set filter product to Default sorting when product change
+    // and store the unchanged product to initialProduct
+    if (productCategory) {
       setSelectFilter("default");
+      setInitialProduct(products);
     }
-  }, [productSize]);
+  }, [productCategory]);
+
   return (
-    <div>
-      <p>Showing all {products && products.length} results</p>
+    <div className={styles.shopFilter__wrapper}>
+      <p className={styles.shopFilter__resultMsg}>
+        Showing all {products && products.length} results
+      </p>
       <SelectField fieldValue={selectFilter} setFieldValue={setSelectFilter}>
         <option value="default">Default sorting</option>
         <option value="avg-rating">Sort by average rating</option>
-        <option value="price-low">Sort by price:low to high</option>
-        <option value="price-high">Sort by price:high to low</option>
+        <option value="price-low">Sort by price: low to high</option>
+        <option value="price-high">Sort by price: high to low</option>
       </SelectField>
     </div>
   );
