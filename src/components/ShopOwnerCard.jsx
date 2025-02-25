a5ce9eca55c5ed598e1cb230ec2eed0baaf96876
zahirul <zahirulopel@gmail.com>
2025-02-25 10:28:57 +0600

    add dicebear api to shopOwnerCard

---
+++ src/components/ShopOwnerCard.jsx
@@ -0,0 +1,19 @@
+import styles from "../styles/components/shopOwnerCard.module.css";
+// dicebear.com api
+const ShopOwnerCard = ({ userData }) => {
+  const fullName = userData.name.firstname + " " + userData.name.lastname;
+  return (
+    <section className={styles.card__container}>
+      <img
+        className={styles.card__img}
+        src={`https://api.dicebear.com/9.x/personas/svg?seed=${userData.name.firstname}&backgroundColor=b6e3f4,c0aede,d1d4f9,ffd5dc,ffdfbf`}
+      />
+      <div className={styles.card__info}>
+        <p>{fullName}</p>
+        <p>{userData.email}</p>
+        <p>{userData.phone}</p>
+      </div>
+    </section>
+  );
+};
+export default ShopOwnerCard;
--- src/page/Contacts.jsx
+++ src/page/Contacts.jsx
@@ -1,5 +1,6 @@
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
+import ShopOwnerCard from "../components/ShopOwnerCard";
 import styles from "../styles/page/contacts.module.css";
 const Contacts = () => {
   return (
@@ -7,7 +8,32 @@ const Contacts = () => {
       <header>
         <Navbar />
       </header>
-      <main></main>
+      <main>
+        <ShopOwnerCard
+          userData={{
+            address: {
+              geolocation: {
+                lat: "-37.3159",
+                long: "81.1496",
+              },
+              city: "kilcoole",
+              street: "new road",
+              number: 7682,
+              zipcode: "12926-3874",
+            },
+            id: 1,
+            email: "john@gmail.com",
+            username: "johnd",
+            password: "m38rmF$",
+            name: {
+              firstname: "john",
+              lastname: "doe",
+            },
+            phone: "1-570-236-7033",
+            __v: 0,
+          }}
+        />
+      </main>
       <Footer />
     </div>
   );
