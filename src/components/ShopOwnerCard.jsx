e87df4489768c0b01abe7df085dd17c3268196d2
zahirul <zahirulopel@gmail.com>
2025-02-26 16:49:41 +0600

    add shopOwnerCard component

--- src/components/ShopOwnerCard.jsx
+++ src/components/ShopOwnerCard.jsx
@@ -3,7 +3,7 @@ import styles from "../styles/components/shopOwnerCard.module.css";
 const ShopOwnerCard = ({ userData }) => {
   const fullName = userData.name.firstname + " " + userData.name.lastname;
   return (
-    <section className={styles.card__container}>
+    <section className={styles.card}>
       <img
         className={styles.card__img}
         src={`https://api.dicebear.com/9.x/personas/svg?seed=${userData.name.firstname}&backgroundColor=b6e3f4,c0aede,d1d4f9,ffd5dc,ffdfbf`}
