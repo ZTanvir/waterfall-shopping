7c5f00633e9e3e6dc6941efb9f66ded339aa2e4f
zahirul <zahirulopel@gmail.com>
2025-02-26 17:52:50 +0600

    add user card to Conacts page

--- src/components/ShopOwnerCard.jsx
+++ src/components/ShopOwnerCard.jsx
@@ -9,9 +9,18 @@ const ShopOwnerCard = ({ userData }) => {
         src={`https://api.dicebear.com/9.x/personas/svg?seed=${userData.name.firstname}&backgroundColor=b6e3f4,c0aede,d1d4f9,ffd5dc,ffdfbf`}
       />
       <div className={styles.card__info}>
-        <p>{fullName}</p>
-        <p>{userData.email}</p>
-        <p>{userData.phone}</p>
+        <p className={styles.card__info__fullName}>
+          <span className={styles.card__info_bold}>Name: </span>
+          {fullName}
+        </p>
+        <p>
+          <span className={styles.card__info_bold}>Email: </span>
+          {userData.email}
+        </p>
+        <p>
+          <span className={styles.card__info_bold}>Phone: </span>
+          {userData.phone}
+        </p>
       </div>
     </section>
   );
--- src/page/Contacts.jsx
+++ src/page/Contacts.jsx
@@ -5,11 +5,20 @@ import userService from "../services/users";
 import Loading from "../components/Loading";
 import styles from "../styles/page/contacts.module.css";
 const Contacts = () => {
+  const { data, isLoading, isError } = userService.getAllUsers();
+
+  if (isLoading)
+    return (
+      <div className={styles.loadingContainer}>
+        <Loading />
+      </div>
+    );
   return (
     <div className={styles.contacts__page}>
       <header>
         <Navbar />
       </header>
+      <main>{data && <ShopOwnerCards cardsData={data} />}</main>
       <Footer />
     </div>
   );
