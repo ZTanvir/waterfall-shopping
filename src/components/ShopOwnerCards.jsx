27d4a464def9e1a8fd3efceafcb293054a7b4a30
zahirul <zahirulopel@gmail.com>
2025-02-26 17:42:22 +0600

    add shopOwnerCards component to show shopOwnerCard

---
+++ src/components/ShopOwnerCards.jsx
@@ -0,0 +1,16 @@
+import ShopOwnerCard from "./ShopOwnerCard";
+import styles from "../styles/components/shopOwnerCards.module.css";
+
+const ShopOwnerCards = ({ cardsData }) => {
+  console.log("cardsData", cardsData);
+
+  return (
+    <div className={styles.cards__container}>
+      {cardsData &&
+        cardsData.map((cardData) => (
+          <ShopOwnerCard key={cardData.id} userData={cardData} />
+        ))}
+    </div>
+  );
+};
+export default ShopOwnerCards;
---
+++ src/services/users.js
@@ -0,0 +1,15 @@
+import axios from "axios";
+import useSWR from "swr";
+const baseUrl = "https://fakestoreapi.com/users";
+const fetcher = (url) => axios.get(url).then((res) => res.data);
+
+const getAllUsers = () => {
+  const { data, isLoading, error } = useSWR(`${baseUrl}`, fetcher);
+  return {
+    data,
+    isLoading,
+    isError: error,
+  };
+};
+
+export default { getAllUsers };
