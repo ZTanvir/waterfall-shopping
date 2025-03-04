f993c076b5786a78153debdd1aedeff7dc37ef2b
zahirul <zahirulopel@gmail.com>
2025-03-04 10:14:54 +0600

    setup react leaflet map

--- src/page/SingleContact.jsx
+++ src/page/SingleContact.jsx
@@ -8,33 +8,36 @@ const SingleContact = ({}) => {
   const { id } = useParams();
   const { data, isLoading, isError } = usersService.getSingleUser(id);
   const fullName = data && data.name.firstname + " " + data.name.lastname;
+  console.log("Data", data);
+
   if (isLoading) return <Loading />;
   return (
-    <div>
+    <div className={styles.singleContact__page}>
       <header>
         <Navbar />
       </header>
       {data && (
         <main>
-          <div>
-            <img
-              src={`https://api.dicebear.com/9.x/personas/svg?seed=${data.name.firstname}&backgroundColor=b6e3f4,c0aede,d1d4f9,ffd5dc,ffdfbf`}
-              alt=""
-            />
-            <div className={styles.card__info}>
-              <p className={styles.card__info__fullName}>
-                <span className={styles.card__info_bold}>Name: </span>
-                {fullName}
-              </p>
-              <p>
-                <span className={styles.card__info_bold}>Email: </span>
-                {data.email}
-              </p>
-              <p>
-                <span className={styles.card__info_bold}>Phone: </span>
-                {data.phone}
-              </p>
+          <div className={styles.user__container}>
+            <div className={styles.user__info__container}>
+              <img
+                className={styles.user__img}
+                src={`https://api.dicebear.com/9.x/personas/svg?seed=${data.name.firstname}&backgroundColor=b6e3f4,c0aede,d1d4f9,ffd5dc,ffdfbf`}
+                alt=""
+              />
+              <div className={styles.card__info}>
+                <p className={styles.card__info__fullName}>{fullName}</p>
+                <p>
+                  <span className={styles.card__info_bold}>Email: </span>
+                  {data.email}
+                </p>
+                <p>
+                  <span className={styles.card__info_bold}>Phone: </span>
+                  {data.phone}
+                </p>
+              </div>
             </div>
+            <div className={styles.user__address}></div>
           </div>
         </main>
       )}
