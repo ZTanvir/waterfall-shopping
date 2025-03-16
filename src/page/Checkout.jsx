e1f0daf4db7f5f884ea031a1f76eba2513bf14ff
zahirul <zahirulopel@gmail.com>
2025-03-16 13:22:47 +0600

    checkout page form added

--- src/page/Checkout.jsx
+++ src/page/Checkout.jsx
@@ -1,13 +1,84 @@
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
+import CartDetails from "../components/CartDetails";
+import Btn from "../components/Btn";
+import styles from "../styles/page/checkout.module.css";
 
 const Checkout = ({ cart }) => {
+  const handleBtn = (e) => {};
   return (
-    <div>
+    <div id={styles.checkoutPage}>
       <header>
         <Navbar cart={cart} />
       </header>
-      <main></main>
+      <main>
+        <div className={styles.formSectionContainer}>
+          <h1 className={styles.formSection__title}>Contact information</h1>
+          <form id={styles.userInfoForm}>
+            <div>
+              <input
+                type="email"
+                name="email"
+                id="email"
+                required
+                placeholder="Email"
+              />
+            </div>
+            <h2>Shipping address</h2>
+            <div className={styles.userNameFields}>
+              <input
+                type="text"
+                name="fname"
+                id="fname"
+                placeholder="First name(optional)"
+              />
+              <input
+                type="text"
+                name="lname"
+                id="lname"
+                required
+                placeholder="Last name"
+              />
+            </div>
+            <div>
+              <input
+                type="text"
+                name="address"
+                id="address"
+                placeholder="Address"
+                required
+              />
+            </div>
+            <div className={styles.userLocationsFields}>
+              <input
+                type="text"
+                name="apartment"
+                id="apartment"
+                placeholder="Apartment"
+                required
+              />
+              <input
+                type="number"
+                name="city"
+                id="city"
+                placeholder="City"
+                required
+              />
+            </div>
+            <div>
+              <input
+                type="tel"
+                name="phone"
+                id="phone"
+                placeholder="Phone"
+                required
+              />
+            </div>
+            <Btn text="Submit" handleBtn={handleBtn} />
+          </form>
+        </div>
+        <div></div>
+      </main>
       <Footer />
     </div>
   );
